b3543637c026bf6dbebab3fcedb331787167189b
Tripod <ind.tripod@gmail.com>
2025-06-13 01:30:24 +0200

    boolean bindings for sqlite fix

--- admin_panel/components/tableController/rowInputs/boolean.tsx
+++ admin_panel/components/tableController/rowInputs/boolean.tsx
@@ -8,7 +8,7 @@ export default function StringInput ({ title, value, onChange }: Props) {
 	return <div className="w-full grid grid-cols-[200px_auto] gap-2 items-center">
 		<span className="grow-1 text-lg truncate">{title}</span>
 		<input type="checkbox" className="p-2 w-[30px] h-[30px]" checked={value} onChange={e => {
-			onChange(e.target.checked);
+			onChange(e.target.checked ? 1 : 0);
 		}}/>
 	</div>
 }
